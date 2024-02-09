631378b7a6bb8c590b9cf99bfd243afa209d0e00
Eduarda Gomes <eduardagomesdesouza8@gmail.com>
2024-02-09 10:57:44 -0300

    Adicionar arquivos de projeto.

---
+++ AppVendasWeb/Migrations/20240209111323_UpdateTableCategoria.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppVendasWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ AppVendasWeb/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppVendasWeb.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriaId { get; set; }
+
+        [Display(Name = "Categoria")]
+        public string CategoriaNome { get; set; }
+    }
+}
